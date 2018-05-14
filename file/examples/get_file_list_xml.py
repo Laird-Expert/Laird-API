041212a223e25b1218b4b36aa8f0b1918fc07a0f
Laird-Assessors <39122566+Laird-Assessors@users.noreply.github.com>
2018-05-14 13:22:58 +0100

    Create get_file_list_xml.py

---
+++ file/examples/get_file_list_xml.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import requests
+
+session = requests.Session()
+
+YOURAPIKEY = "INSERT API KEY"
+FILEID = "FILEID"
+
+headers = {"User-Agent":"Laird Assessors API Example","Connection":"close","Accept-Encoding":"gzip, deflate","Accept":"*/*"}
+response = session.get("https://test2-lairdassessors.swiftcase.co.uk/api/v2/"+YOURAPIKEY+"/file/"+FILEID+".xml", headers=headers)
+
+print("Status code:   %i" % response.status_code)
+print("Response body: %s" % response.content)
