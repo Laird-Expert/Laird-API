f1d1485528aaae02d0fff7612310a154435a44f7
Laird-Assessors <39122566+Laird-Assessors@users.noreply.github.com>
2018-08-15 12:45:21 +0100

    Update avaliblitly_json.py

--- task/examples/avaliblitly_json.py
+++ task/examples/avaliblitly_json.py
@@ -3,7 +3,7 @@ import requests
 session = requests.Session()
 
 headers = {"Cache-Control":"no-cach","Content-Type":"application/x-www-form-urlencoded"}
-response = session.get("https://test2-lairdassessors.swiftcase.co.uk/api/v2/YOURAPIKEY/task/TASKID/availability/DATEFROM/MAXRESULTS.json", headers=headers)
+response = session.get("https://test-lairdassessors.swiftcase.co.uk/api/v2/YOURAPIKEY/task/TASKID/availability/DATEFROM/MAXRESULTS.json", headers=headers)
 
 print("Status code:   %i" % response.status_code)
 print("Response body: %s" % response.content)
