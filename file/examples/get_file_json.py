37670dfe99bdf6cbae42bdf21811cd9927d7d55a
Laird-Assessors <39122566+Laird-Assessors@users.noreply.github.com>
2018-08-15 12:44:37 +0100

    Update get_file_json.py

--- file/examples/get_file_json.py
+++ file/examples/get_file_json.py
@@ -8,7 +8,7 @@ YOURAPIKEY = "INSERT API KEY"
 FILEID = "FILEID"
 
 headers = {"User-Agent":"Laird Assessors API Example","Connection":"close","Accept-Encoding":"gzip, deflate","Accept":"*/*"}
-response = session.get("https://test2-lairdassessors.swiftcase.co.uk/api/v2/"+YOURAPIKEY+"/file/"+FILEID+".json", headers=headers)
+response = session.get("https://test-lairdassessors.swiftcase.co.uk/api/v2/"+YOURAPIKEY+"/file/"+FILEID+".json", headers=headers)
 
 print("Status code:   %i" % response.status_code)
 print("Response body: %s" % response.content)
