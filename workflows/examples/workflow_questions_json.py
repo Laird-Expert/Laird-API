8e5386e9cd86dc67c9504299a97b31b86bb3e8fa
Laird-Assessors <39122566+Laird-Assessors@users.noreply.github.com>
2018-08-15 12:43:17 +0100

    Update workflow_questions_json.py

--- workflows/examples/workflow_questions_json.py
+++ workflows/examples/workflow_questions_json.py
@@ -7,7 +7,7 @@ session = requests.Session()
 YOURAPIKEY = "INSERT API KEY"
 
 headers = {"User-Agent":"Laird Assessors API Example","Connection":"close","Accept-Encoding":"gzip, deflate","Accept":"*/*"}
-response = session.get("https://test2-lairdassessors.swiftcase.co.uk/api/v2/"+YOURAPIKEY+"/workflow/70.json", headers=headers)
+response = session.get("https://test-lairdassessors.swiftcase.co.uk/api/v2/"+YOURAPIKEY+"/workflow/70.json", headers=headers)
 
 print("Status code:   %i" % response.status_code)
 print("Response body: %s" % response.content)
