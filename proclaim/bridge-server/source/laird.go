09f90025896f8fe91cd61c2c589d2659f486b69a
Laird-Assessors <39122566+Laird-Assessors@users.noreply.github.com>
2018-12-11 11:32:12 +0000

    Update laird.go

--- proclaim/bridge-server/source/laird.go
+++ proclaim/bridge-server/source/laird.go
@@ -11,6 +11,7 @@ package main
 
 import (
 	"bytes"
+
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -37,6 +38,300 @@ func logapi() {
 
 
 }
+
+
+
+
+
+func lgetfiles(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	TASK := r.FormValue("TASK")
+	enableCors(&w)
+
+	genurl := "https://test-lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/task/"+TASK+"/files.xml"
+
+	req, err := http.NewRequest("GET", genurl, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Host = "lairdassessors.swiftcase.co.uk"
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("User-Agent", "Laird API Bridge GoLang")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Connection", "close")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 200 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL: "+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+}
+
+func tgetfiles(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	TASK := r.FormValue("TASK")
+	enableCors(&w)
+
+	genurl := "https://test-lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/task/"+TASK+"/files.xml"
+
+	req, err := http.NewRequest("GET", genurl, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Host = "lairdassessors.swiftcase.co.uk"
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("User-Agent", "Laird API Bridge GoLang")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Connection", "close")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 200 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL: "+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func lgetfile(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	FILEID := r.FormValue("FILEID")
+	enableCors(&w)
+
+	genurl := "https://lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/file/"+FILEID+".xml"
+
+	req, err := http.NewRequest("GET", genurl, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Host = "lairdassessors.swiftcase.co.uk"
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("User-Agent", "Laird API Bridge GoLang")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Connection", "close")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 200 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL: "+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+}
+
+
+
+func tgetfile(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	FILEID := r.FormValue("FILEID")
+	enableCors(&w)
+
+	genurl := "https://test-lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/file/"+FILEID+".xml"
+
+	req, err := http.NewRequest("GET", genurl, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Host = "lairdassessors.swiftcase.co.uk"
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("User-Agent", "Laird API Bridge GoLang")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Connection", "close")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 200 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL: "+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+func lbengineer(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	TASK := r.FormValue("TASK")
+	BOOKID := r.FormValue("BOOKID")
+	IDATE := r.FormValue("IDATE")
+
+
+	var toppart = `<?xml version="1.0" encoding="UTF-8"?>`
+	rawXmlData := "<resourceAvailability> <id>"+BOOKID+"</id> <availability> <date>"+IDATE+"</date> </availability> </resourceAvailability>"
+
+	joineddata := []byte(""+toppart+""+rawXmlData+"")
+
+	xmlpost := bytes.NewBuffer(joineddata)
+
+
+	genurl := "https://test-lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/task/"+TASK+"/allocate-resource.xml"
+
+	req, _ := http.NewRequest("POST", genurl, xmlpost)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 201 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL:"+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+
+
+}
+
+
+func tbengineer(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	TASK := r.FormValue("TASK")
+	BOOKID := r.FormValue("BOOKID")
+	IDATE := r.FormValue("IDATE")
+
+
+	var toppart = `<?xml version="1.0" encoding="UTF-8"?>`
+	rawXmlData := "<resourceAvailability> <id>"+BOOKID+"</id> <availability> <date>"+IDATE+"</date> </availability> </resourceAvailability>"
+
+	joineddata := []byte(""+toppart+""+rawXmlData+"")
+
+	xmlpost := bytes.NewBuffer(joineddata)
+
+
+	genurl := "https://test-lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/task/"+TASK+"/allocate-resource.xml"
+
+	req, _ := http.NewRequest("POST", genurl, xmlpost)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 201 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL:"+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+
+
+}
+
+
 
 
 func ttask(w http.ResponseWriter, r *http.Request) {
@@ -81,6 +376,94 @@ func ttask(w http.ResponseWriter, r *http.Request) {
 
 
 }
+
+func lsendfile(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	TASK := r.FormValue("TASK")
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	xmlpost := bytes.NewReader(b)
+
+	genurl := "https://lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/task/"+TASK+"/files.xml"
+
+	req, _ := http.NewRequest("POST", genurl, xmlpost)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 201 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL:"+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+
+
+}
+
+func tsendfile(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	TASK := r.FormValue("TASK")
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	xmlpost := bytes.NewReader(b)
+
+	genurl := "https://test-lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/task/"+TASK+"/files.xml"
+
+	req, _ := http.NewRequest("POST", genurl, xmlpost)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 201 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL:"+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+
+
+}
+
+
+
+
 
 func ltask(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
@@ -351,7 +734,7 @@ func lengineer(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	APIKEY := r.FormValue("APIKEY")
-	TASK := r.FormValue("TASk")
+	TASK := r.FormValue("TASK")
 	IDATE := r.FormValue("IDATE")
 	MAXRESULT := r.FormValue("MAXRESULT")
 	enableCors(&w)
@@ -391,7 +774,98 @@ func lengineer(w http.ResponseWriter, r *http.Request) {
 }
 
 
+func ttaskdetails(w http.ResponseWriter, r *http.Request) {
 
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	TASK := r.FormValue("TASK")
+	enableCors(&w)
+
+
+	genurl := "https://test-lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/task/"+TASK+".xml"
+
+	req, err := http.NewRequest("GET", genurl, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Host = "test-lairdassessors.swiftcase.co.uk"
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("User-Agent", "Laird API Bridge GoLang")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Connection", "close")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 200 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL:"+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+
+}
+
+
+func ltaskdetails(w http.ResponseWriter, r *http.Request) {
+
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	APIKEY := r.FormValue("APIKEY")
+	TASK := r.FormValue("TASK")
+	enableCors(&w)
+
+
+	genurl := "https://lairdassessors.swiftcase.co.uk/api/v2/"+APIKEY+"/task/"+TASK+".xml"
+
+	req, err := http.NewRequest("GET", genurl, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Host = "test-lairdassessors.swiftcase.co.uk"
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("User-Agent", "Laird API Bridge GoLang")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Connection", "close")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.StatusCode == 200 {
+		w.Header().Set("Content-Type", "application/xml")
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	fmt.Printf("Laird API URL:"+genurl+"\n")
+	fmt.Printf("Laird API Status Code Response: %d\n", resp.StatusCode)
+	logapi()
+	responseString := string(responseData)
+	fmt.Fprint(w, responseString)
+
+}
 
 
 
@@ -405,12 +879,22 @@ func main() {
 	http.HandleFunc("/test/workflow", tworkflows)
 	http.HandleFunc("/test/status", tstatus)
 	http.HandleFunc("/test/engineer", tengineer)
+	http.HandleFunc("/test/bookengineer", tbengineer)
 	http.HandleFunc("/test/task", ttask)
+	http.HandleFunc("/test/taskdetails", ttaskdetails)
+	http.HandleFunc("/test/sendfile", tsendfile)
+	http.HandleFunc("/test/getfile", tgetfile)
+	http.HandleFunc("/test/getfiles", tgetfiles)
 
 	http.HandleFunc("/live/workflow", lworkflows)
 	http.HandleFunc("/live/status", lstatus)
 	http.HandleFunc("/live/engineer", lengineer)
+	http.HandleFunc("/live/bookengineer", lbengineer)
 	http.HandleFunc("/live/task", ltask)
+	http.HandleFunc("/live/taskdetails", ltaskdetails)
+	http.HandleFunc("/live/sendfile", lsendfile)
+	http.HandleFunc("/live/getfile", lgetfile)
+	http.HandleFunc("/live/getfiles", lgetfiles)
 
 
 
